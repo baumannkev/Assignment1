c37b0cb95f6647b765d3ccb3e40733032c2f7f4d
Kevin Baumann <baumannkev@gmail.com>
2022-09-15 17:35:55 -0700

    Initial commit

---
+++ Assets/Ball.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Ball : MonoBehaviour
+{
+    public float x = 0.0f;
+    public float y = 0.0f;
+    public float z = 0.0f;
+    
+    public GameObject ball;
+    public string HitMessage = "Hit collider!";
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player") {
+            x *= -1.0f;
+            y *= -1.0f;
+            Debug.Log(HitMessage);
+        }
+        
+        if (collision.gameObject.tag == "Game Controller") {
+            y *= -1.0f;
+            Debug.Log(HitMessage);
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        x = -0.1f;
+        y = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(x, y, z);
+    }
+}
